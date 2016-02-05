5d6606ee181c7fa3ae5405e6bf50a6031de7e6c7
qitian <qitianchan@sina.com>
2016-02-05 15:44:03 +0800

    地图显示

---
+++ midstation/static/js/map/all_devices.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var map = new AMap.Map('container', {
+        resizeEnable: true,
+        center: [116.397428, 39.90923],
+        zoom: 13
+    });
+    map.clearMap();  // 清除地图覆盖物
+    var markers = [{
+        icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b1.png',
+        position: [116.205467, 39.907761]
+    }, {
+        icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b2.png',
+        position: [116.368904, 39.913423]
+    }, {
+        icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b3.png',
+        position: [116.305467, 39.807761]
+    }];
+    // 添加一些分布不均的点到地图上,地图上添加三个点标记，作为参照
+    markers.forEach(function(marker) {
+        new AMap.Marker({
+            map: map,
+            icon: marker.icon,
+            position: [marker.position[0], marker.position[1]],
+            offset: new AMap.Pixel(-12, -36)
+        });
+    });
+    var newCenter = map.setFitView();
+    // 添加事件监听, 使地图自适应显示到合适的范围
+    AMap.event.addDomListener(document.getElementById('setFitView'), 'click', function() {
+        var newCenter = map.setFitView();
+    });
+});
